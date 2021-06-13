1ee09287fa33124f9069ec2ad04570b0ad6e38c2
Chris <mistercwood@gmail.com>
2021-06-13 20:30:26 +1000

    Completed exercise successfully, all tests passing

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanArr = Array.from(str).filter(char => char.match(/[a-z]/i)); 
+    const revArr = [...cleanArr].reverse();
+    return (cleanArr.join('').toLowerCase() === revArr.join('').toLowerCase()) ? true : false;
 };
 
 module.exports = palindromes;
+
+// * Create array from string with all punctuation and spaces removed, reduced to lowercase.
+// * Make copy of array, and reverse it
+// * Ternary to return true if both arrays are the same and false if they are not.
