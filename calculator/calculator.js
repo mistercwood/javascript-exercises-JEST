055df62e8625a11e1ec200709ee916dbabd9700f
Chris <mistercwood@gmail.com>
2021-06-13 20:31:03 +1000

    Completed exercise successfully, all tests passing.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length < 1) {
+    return 0;
+  } 
+  else {
+    let num = 0;
+	  arr.forEach(item => (num += item))
+    return num;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    let num = 1;
+    arr.forEach(item => (num *= item))
+    return num;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  } 
+  else {
+    let factorial = 1;
+    for (i = 1; i < num + 1; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
 };
 
 module.exports = {
