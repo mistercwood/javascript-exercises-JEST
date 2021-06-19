726afc313f6a559b55724dccce125a6a3d9c1deb
Chris <mistercwood@gmail.com>
2021-06-19 10:52:40 +1000

    Exercise completed successfully, all tests passing

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    const sequence = [0, 1];
+    if (member < 0) {
+        return "OOPS"
+    }
+    else {
+      for (let i = 1; i < member; i++) {
+        let newMember = parseInt(sequence[sequence.length - 2]) + parseInt(sequence[sequence.length - 1]);
+        sequence.push(newMember);
+        }
+    return sequence[sequence.length - 1];
+    }
 };
 
 module.exports = fibonacci;
