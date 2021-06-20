6492e74b6046a10d19051b6249dca22694fec7be
Chris <mistercwood@gmail.com>
2021-06-20 17:47:51 +1000

    Completed exercise successfully, all tests passing ok.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    const oldest = obj.sort(function(a, b){
+        const last = (a.yearOfDeath ? a.yearOfDeath : (new Date()).getFullYear()) - a.yearOfBirth;
+        const next = (b.yearOfDeath ? b.yearOfDeath : (new Date()).getFullYear()) - b.yearOfBirth;
+        return last > next ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 module.exports = findTheOldest;
+
+
